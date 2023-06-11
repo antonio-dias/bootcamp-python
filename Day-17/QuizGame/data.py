3bfcf7583d4e78568b5c56fbcd2a9cd7c09f2257
Antonio Dias <antonio.dias@neogrid.com>
2023-06-11 19:39:12 -0300

    Day 17 - Intermediate - The Quiz Project & the Benefits of OOP

---
+++ Day-17/QuizGame/data.py
@@ -0,0 +1,41 @@
+# Questions get from Open Trivia Database (https://opentdb.com/api_config.php)
+
+question_data = [
+    {
+        "category": "Science & Nature",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Igneous rocks are formed by excessive heat and pressure.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Music", "type": "boolean",
+        "difficulty": "easy",
+        "question": "The music group Daft Punk got their name from a negative review they recieved.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "The Great Wall of China is visible from the moon.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Music", "type": "boolean", "difficulty": "easy",
+     "question": "Eurobeat is primarily an Italian genre of music.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Geography", "type": "boolean", "difficulty": "easy",
+     "question": "There is a city called Rome in every continent on Earth.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Politics", "type": "boolean", "difficulty": "easy",
+     "question": "Despite being seperated into multiple countries, The Scandinavian countries are unified by all having the same monarch.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+     "question": "Minecraft can be played with a virtual reality headset.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+     "question": "Actor Tommy Chong served prison time.", "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"category": "Science: Mathematics", "type": "boolean", "difficulty": "easy",
+     "question": "A scalene triangle has two sides of equal length.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Music", "type": "boolean", "difficulty": "easy",
+     "question": "John Williams composed the music for &quot;Star Wars&quot;.",
+     "correct_answer": "True", "incorrect_answers": ["False"]}
+]
